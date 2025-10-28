6505843c0633c6b7297d4acef25151c2b6e5b410
TimHendrikMsn <tim.hps.maassen@gmail.com>
2025-10-28 10:10:03 +0100

    Create a main.py file to start a chat in the terminal, better UI needed

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from src.agent import stream_agent_response
+
+
+def main():
+    while True:
+        user_input = input("You:\t").strip()
+        if user_input.lower() in ["exit", "quit"]:
+            print("👋 Goodbye!\n")
+            break
+        thread_id = "interactive_thread"
+        print("Model starts streaming...")
+        for chunk in stream_agent_response(user_input, thread_id):
+            if chunk.step == "model" and chunk.content.type == "text":
+                print(f"\nAgent:\t{chunk.content.text}\n")
+            elif chunk.content.type == "tool_call":
+                print(f"\t- Tool Call:\n\t\t- Tool: {chunk.content.name}\n\t\t- Args: {chunk.content.args}\n")
+            elif chunk.step == "tools":
+                print(f"\t- Tool Response:\n\t\t- Content: {chunk.content.text}\n")
+            else:
+                print(f"\t- {chunk.step}:\n\t\t- Content: {chunk.content}\n")
+
+if __name__ == "__main__":
+    main()
--- app/src/agent.py
+++ app/src/agent.py
@@ -27,9 +27,39 @@ agent = create_agent(
     model=settings.model_name,
     system_prompt=load_yaml_prompt(settings.system_prompt_path),
     tools=[
+        build_regex,
+        run_regex
     ],
     checkpointer=InMemorySaver(), 
     middleware=[tool_call_limit]
     )
 
-     
+def stream_agent_response(user_input: str, thread_id: str):
+    for chunk in agent.stream(  
+        {"messages": [{"role": "user", "content": user_input}]},
+        stream_mode="updates",
+        config={"thread_id": thread_id}
+    ):
+        for step, data in chunk.items():
+            if "Middleware" in step:
+                continue
+
+            content = data['messages'][-1].content_blocks
+
+            if content and content[0]["type"] == "tool_call":
+                tool_call_data = content[0]
+                content = ToolCall(
+                    type="tool_call",
+                    name=tool_call_data.get("name"),
+                    args=tool_call_data.get("args"),
+                    id=tool_call_data.get("id")
+                )
+            else:
+                content = ModelResponseContent(type="text", text=content[0].get("text") if content else None)
+
+            
+            yield StreamResponse(step=step, content=content)
+
+    
+
+                
